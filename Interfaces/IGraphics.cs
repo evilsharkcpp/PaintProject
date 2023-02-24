e45459c00eb399766691aa6175dffe9db306135b
Asaulyuk Valentin <49671793+evilsharkcpp@users.noreply.github.com>
2023-02-24 15:58:12 +0700

    Update IGraphics.cs
    Added outline flag for draw outline.

--- Interfaces/IGraphics.cs
+++ Interfaces/IGraphics.cs
@@ -5,12 +5,12 @@ namespace Interfaces
     public interface IGraphics
     {
         IDrawable Style { get; }
-        void DrawLine(Vector2 v1, Vector2 v2);
-        void DrawRect(Vector2 v1, Vector2 v2);
-        void DrawTriangle(Vector2 v1, Vector2 v2, Vector2 v3);
-        void DrawCircle(Vector2 center, double radius, bool isFill);
-        void DrawEllipse(Vector2 center, double a, double b, bool isFill);
-        void DrawPolygon(IEnumerable<Vector2> points);
+        void DrawLine(Vector2 v1, Vector2 v2, bool isFill, bool isOutLine);
+        void DrawRect(Vector2 v1, Vector2 v2, bool isFill, bool isOutLine);
+        void DrawTriangle(Vector2 v1, Vector2 v2, Vector2 v3, bool isFill, bool isOutLine);
+        void DrawCircle(Vector2 center, double radius, bool isFill, bool isOutLine);
+        void DrawEllipse(Vector2 center, double a, double b, bool isFill, bool isOutLine);
+        void DrawPolygon(IEnumerable<Vector2> points, bool isFill, bool isOutLine);
 
     }
 }
