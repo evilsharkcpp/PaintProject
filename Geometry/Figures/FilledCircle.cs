a4e7de1629d97b680c996190e87c9b78e8f37c61
Juliya Zatolockaya <zatolockaya14@gmail.com>
2023-02-25 15:25:30 +0700

    Added classes Circle and FilledCircle

---
+++ Geometry/Figures/FilledCircle.cs
@@ -0,0 +1,27 @@
+﻿using DataStructures.Geometry;
+using Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geometry.Figures
+{
+    internal class FilledCircle : Circle
+    {
+        public FilledCircle(Circle circle) : base(circle)
+        {
+        }
+
+        public FilledCircle(Point2d center, double radius) : base(center, radius)
+        {
+        }
+
+        public override void Draw(IGraphics graphics)
+        {
+
+            graphics.DrawCircle(_center, _radius, true, true);
+        }
+    }
+}
