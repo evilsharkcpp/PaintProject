2942efc27de06bda1f38952d464eca83dd1e26e8
Asaulyuk Valentin <49671793+evilsharkcpp@users.noreply.github.com>
2023-02-26 18:13:31 +0700

    Moved VM in Logic

--- GUI(WPF)/App.xaml.cs
+++ GUI(WPF)/App.xaml.cs
@@ -1,11 +1,4 @@
-﻿using GUI_WPF.ViewModels;
-using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 
 namespace GUI_WPF_
 {
