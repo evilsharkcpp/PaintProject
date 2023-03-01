e208e544a7ba0f40424f0bef175955725a041489
Asaulyuk Valentin <49671793+evilsharkcpp@users.noreply.github.com>
2023-03-01 17:18:01 +0700

    Update IGraphics.cs

--- Interfaces/IGraphics.cs
+++ Interfaces/IGraphics.cs
@@ -6,7 +6,6 @@ namespace Interfaces
     {
         IDrawable Style { get; }
         void DrawLine(Vector2 v1, Vector2 v2, bool isFill, bool isOutLine);
-        void DrawRect(Vector2 v1, Vector2 v2, bool isFill, bool isOutLine);
         void DrawTriangle(Vector2 v1, Vector2 v2, Vector2 v3, bool isFill, bool isOutLine);
         void DrawCircle(Vector2 center, double radius, bool isFill, bool isOutLine);
         void DrawEllipse(Vector2 center, double a, double b, bool isFill, bool isOutLine);
