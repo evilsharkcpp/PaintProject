59fa4779535c0ad94c87708eba566a7818e576e8
Диана Сотникова <dianasotnikova01@gmail.com>
2023-03-04 14:27:20 +0700

    пример теста

--- Tests(xUnit)/UnitTest1.cs
+++ Tests(xUnit)/UnitTest1.cs
@@ -1,3 +1,7 @@
+using DataStructures.Geometry;
+using Geometry.Figures;
+using Interfaces;
+
 namespace Tests_xUnit_
 {
     public class UnitTest1
@@ -5,7 +9,14 @@ namespace Tests_xUnit_
         [Fact]
         public void Test1()
         {
-
+			var fabric = FigureFabric.Create();
+            IFigure? figure = fabric?.CreateFigure("Line", new Point2d(0, 0), new Point2d(10, 10));
+            var point1 = figure?.PointParameters.Where(q => q.Name == "Point1").First();
+            var point2 = figure?.PointParameters.Where(q => q.Name == "Point2").First();
+            Assert.Equal(0, point1?.Value.X);
+            Assert.Equal(0, point1?.Value.Y);
+            Assert.Equal(10, point2?.Value.X);
+            Assert.Equal(10, point2?.Value.Y);
         }
     }
 }
