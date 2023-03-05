146b85b79dd04b869de35ce57dcb1f1b13679947
Игорь Ларин <i.larin.2019@azure.nstu.ru>
2023-03-06 00:02:35 +0700

    Еще раз

--- GUI(WPF)/Graphics/Graphic.cs
+++ GUI(WPF)/Graphics/Graphic.cs
@@ -1,58 +1,96 @@
 ﻿using DataStructures.Geometry;
+using Geometry.Figures;
 using Interfaces;
 using System.Collections.Generic;
 using System.Numerics;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
 using System.Windows.Shapes;
 
 namespace GUI_WPF.Graphics
 {
-    public class Graphic : IGraphics
-    {
-        public IDrawable GraphicStyle { get; set; }
+   public class Graphic : IGraphics
+   {
+      public IDrawable GraphicStyle { get; set; }
 
-        private Canvas _canvas;
+      private Canvas _canvas;
 
-        public void DrawCircle(Point2d center, double radius, bool isFill, bool isOutLine)
-        {
-        }
+      public void DrawEllipse(Point2d start, double a, double b, bool isFill, bool isOutLine)
+      {
+         var elllipse = new Ellipse();
+         elllipse.Height = a;
+         elllipse.Width = b;
+         var c = new Color();
+         c.A = GraphicStyle.OutLineColor.A;
+         c.R = GraphicStyle.OutLineColor.R;
+         c.G = GraphicStyle.OutLineColor.G;
+         c.B = GraphicStyle.OutLineColor.B;
+         elllipse.Stroke = new SolidColorBrush(c);
+         Canvas.SetLeft(elllipse, start.X);
+         Canvas.SetTop(elllipse, start.Y);
+         _canvas.Children.Add(elllipse);
+      }
 
-        public void DrawEllipse(Point2d center, double a, double b, bool isFill, bool isOutLine)
-        {
-            throw new System.NotImplementedException();
-        }
+      public void DrawLine(Point2d v1, Point2d v2, bool isFill, bool isOutLine)
+      {
+         var line = new System.Windows.Shapes.Line();
+         line.X1 = v1.X;
+         line.Y1 = v1.Y;
+         line.X2 = v2.X;
+         line.Y2 = v2.Y;
+         var c = new Color();
+         c.A = GraphicStyle.OutLineColor.A;
+         c.R = GraphicStyle.OutLineColor.R;
+         c.G = GraphicStyle.OutLineColor.G;
+         c.B = GraphicStyle.OutLineColor.B;
+         line.Stroke = new SolidColorBrush(c);
+         _canvas.Children.Add(line);
+      }
 
-        public void DrawLine(Point2d v1, Point2d v2, bool isFill, bool isOutLine)
-        {
-            var line = new Line();
-            line.X1 = v1.X;
-            line.Y1 = v1.Y;
-            line.X2 = v2.X;
-            line.Y2 = v2.Y;
-            var c = new Color();
-            c.A = GraphicStyle.OutLineColor.A;
-            c.R = GraphicStyle.OutLineColor.R;
-            c.G = GraphicStyle.OutLineColor.G;
-            c.B = GraphicStyle.OutLineColor.B;
-            line.Stroke = new SolidColorBrush(c);
-            _canvas.Children.Add(line);
-        }
+      public void DrawPolygon(IEnumerable<Point2d> points, bool isFill, bool isOutLine)
+      {
+         Polygon poly = new Polygon();
+         PointCollection polyPoints = new PointCollection();
+         foreach (var p in points)         
+            polyPoints.Add(new Point(p.X, p.Y));
+         poly.Points = polyPoints;
+         var c = new Color();
+         c.A = GraphicStyle.OutLineColor.A;
+         c.R = GraphicStyle.OutLineColor.R;
+         c.G = GraphicStyle.OutLineColor.G;
+         c.B = GraphicStyle.OutLineColor.B;
+         poly.Stroke = new SolidColorBrush(c);
+         _canvas.Children.Add(poly);
+      }
 
-        public void DrawPolygon(IEnumerable<Point2d> points, bool isFill, bool isOutLine)
-        {
-            throw new System.NotImplementedException();
-        }
+      public void DrawTriangle(Point2d v1, Point2d v2, Point2d v3, bool isFill, bool isOutLine)
+      {
+         List<Point2d> points = new List<Point2d> { v1, v2, v3 };
+         DrawPolygon(points, isFill, isOutLine);
+      }
 
-        public void DrawTriangle(Point2d v1, Point2d v2, Point2d v3, bool isFill, bool isOutLine)
-        {
-            throw new System.NotImplementedException();
-        }
-        public Graphic() { }
+      public void DrawRectangle(Point2d start, double a, double b, bool isFill, bool isOutLine)
+      {
+         var rect = new Rectangle();
+         rect.Height = a;
+         rect.Width = b;
+         var c = new Color();
+         c.A = GraphicStyle.OutLineColor.A;
+         c.R = GraphicStyle.OutLineColor.R;
+         c.G = GraphicStyle.OutLineColor.G;
+         c.B = GraphicStyle.OutLineColor.B;
+         rect.Stroke = new SolidColorBrush(c);
+         Canvas.SetLeft(rect, start.X);
+         Canvas.SetTop(rect, start.Y);
+         _canvas.Children.Add(rect);
+      }
 
-        public Graphic(Canvas canvas)
-        {
-            _canvas = canvas;
-        }
-    }
+      public Graphic() { }
+
+      public Graphic(Canvas canvas)
+      {
+         _canvas = canvas;
+      }
+   }
 }
--- Interfaces/IGraphics.cs
+++ Interfaces/IGraphics.cs
@@ -1,16 +1,15 @@
 ﻿using DataStructures.Geometry;
-using System.Numerics;
 
 namespace Interfaces
 {
-    public interface IGraphics
-    {
-        IDrawable GraphicStyle { get; set; }
-        void DrawLine(Point2d v1, Point2d v2, bool isFill, bool isOutLine);
-        void DrawTriangle(Point2d v1, Point2d v2, Point2d v3, bool isFill, bool isOutLine);
-        void DrawCircle(Point2d center, double radius, bool isFill, bool isOutLine);
-        void DrawEllipse(Point2d center, double a, double b, bool isFill, bool isOutLine);
-        void DrawPolygon(IEnumerable<Point2d> points, bool isFill, bool isOutLine);
+   public interface IGraphics
+   {
+      IDrawable GraphicStyle { get; set; }
+      void DrawLine(Point2d v1, Point2d v2, bool isFill, bool isOutLine);
+      void DrawTriangle(Point2d v1, Point2d v2, Point2d v3, bool isFill, bool isOutLine);
+      void DrawEllipse(Point2d start, double a, double b, bool isFill, bool isOutLine);
+      void DrawPolygon(IEnumerable<Point2d> points, bool isFill, bool isOutLine);
+      void DrawRectangle(Point2d start, double a, double b, bool isFill, bool isOutLine);
 
-    }
+   }
 }
