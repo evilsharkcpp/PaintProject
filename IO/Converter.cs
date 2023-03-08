a065da4f32ca4f6770ccd00efec3f2ae9fe7a8e8
Nikita <nikitalebedeff@gmail.com>
2023-03-08 18:04:46 +0700

    Теперь точно добавлена запись в JSON

--- IO/Converter.cs
+++ IO/Converter.cs
@@ -1,12 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+
+using Interfaces;
 
 namespace IO
 {
-    internal class Class1
+    internal class ConvertToJSON : IConverter
     {
+        public IEnumerable<IFigure> ReadFile(string filename)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void WriteFile(string filename, IEnumerable<IFigure> figures)
+        {
+            string json_string = JsonConvert.SerializeObject(figures);
+
+            using (StreamWriter writer = new StreamWriter(filename))
+            {
+                writer.Write(json_string);
+            }
+        }
+
     }
 }
