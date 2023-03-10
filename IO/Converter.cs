9deda61eed9acb4169703821d312a2ec09251524
Nikita <nikitalebedeff@gmail.com>
2023-03-10 22:33:32 +0700

    Сериализация массива фигур

--- IO/Converter.cs
+++ IO/Converter.cs
@@ -10,13 +10,22 @@ using Newtonsoft.Json;
 using Interfaces;
 using System.Runtime.Serialization.Json;
 using Splat.ModeDetection;
+using System.Runtime.ConstrainedExecution;
+using DynamicData;
+using System.Runtime.Serialization;
+using System.Xml;
 
 namespace IO
 {
-    internal class ConvertToJSON : IConverter
+    internal class JSONConverter : IConverter
     {
         public IEnumerable<IFigure> ReadFile(string filename)
         {
+            FileStream fs = new FileStream(fileName, FileMode.Open);
+            XmlDictionaryReader reader =
+                XmlDictionaryReader.CreateTextReader(fs, new XmlDictionaryReaderQuotas());
+            DataContractSerializer ser = new DataContractSerializer(typeof(Person));
+
             throw new NotImplementedException();
         }
 
@@ -29,6 +38,27 @@ namespace IO
                 DataContractJsonSerializer ser = new DataContractJsonSerializer(typeof(IFigure));
                 ser.WriteObject(stream, figure);
             }
+        }
+
+    }
+
+    internal class SVGConverter : IConverter
+    {
+        public IEnumerable<IFigure> ReadFile(string filename)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void WriteFile(string filename, IEnumerable<IFigure> figures)
+        {
+
+
+            foreach (IFigure figure in figures)
+            {
+
+
+            }
+
         }
 
     }
