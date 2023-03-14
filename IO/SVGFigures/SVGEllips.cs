f882a98015f26501263fa2e8ca2efc4a679e37a2
Dudnkov <iladudnikov13@gmail.com>
2023-03-14 18:33:50 +0700

    Update SVGEllips.cs

--- IO/SVGFigures/SVGEllips.cs
+++ IO/SVGFigures/SVGEllips.cs
@@ -1,12 +1,27 @@
-﻿using System;
+﻿using Svg;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Drawing;
 
 namespace IO.SVGFigures
 {
     public class SVGEllips
     {
+        public SvgEllipse Ellips(double cx, double cy, double rx, double ry)
+        {
+
+            return new SvgEllipse
+            {
+                CenterX = (SvgUnit)cx,
+                CenterY = (SvgUnit)cy,
+                RadiusX = (SvgUnit)rx,
+                RadiusY = (SvgUnit)ry,
+
+                Stroke = new SvgColourServer(Color.Black)
+            };
+        }
     }
 }
