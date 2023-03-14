f255ae107eaf27b32acb69a74751986d13e7cb9a
Dudnkov <iladudnikov13@gmail.com>
2023-03-14 18:34:18 +0700

    Update SVGCicle.cs

--- IO/SVGFigures/SVGCicle.cs
+++ IO/SVGFigures/SVGCicle.cs
@@ -1,12 +1,26 @@
-﻿using System;
+﻿using Svg;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Drawing;
 
 namespace IO.SVGFigures
 {
-    public class SVGCicle
+    public class SVGCircle
     {
+        public SvgCircle Line(double cx, double cy, double r)
+        {
+
+            return new SvgCircle
+            {
+                CenterX = (SvgUnit)cx,
+                CenterY = (SvgUnit)cy,
+                Radius = (SvgUnit)r,
+
+                Stroke = new SvgColourServer(Color.Black)
+            };
+        }
     }
 }
