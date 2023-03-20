43c76eaeedbcedd42c46d970a6d31a4544f6c8ed
Vladislav <zalevskiy.950@gmail.com>
2023-03-20 20:03:10 +0700

    Добавлен метод для получения Figure из данных ConvertibleFigure

--- Geometry/FigureFabric.cs
+++ Geometry/FigureFabric.cs
@@ -1,5 +1,10 @@
-﻿using Geometry.Attributes;
+﻿using DataStructures;
+using DataStructures.ConvertibleFigures;
+using DataStructures.Geometry;
+using Geometry.Attributes;
+using Geometry.Figures;
 using Interfaces;
+using Splat;
 using System.Reflection;
 
 namespace Geometry
@@ -50,6 +55,29 @@ namespace Geometry
 
                 }
             }
+
+            return figure;
+        }
+
+        public IFigure CreateFigureFromConvertibleFigure(ConvertibleFigure c_figure)
+        {
+            IFigure figure;
+
+
+            figure = CreateFigure(c_figure.GetType().Name);
+
+            double width = c_figure.Width;
+            double heigth = c_figure.Height;
+
+            figure.Size = new Vector2d(width, heigth);
+
+            // Перенос точки позиции из левого верхнего угла в левый нижний
+            Point2d c_position = c_figure.position;
+            c_position.Y += c_figure.Height;
+
+            figure.Position = c_position;
+            figure.Angle = c_figure.angle;
+
 
             return figure;
         }
