28afcdc23109ed7156983cca82f088c4e0e032b3
Asaulyuk Valentin <49671793+evilsharkcpp@users.noreply.github.com>
2023-03-21 00:16:10 +0700

    Added files

---
+++ DataStructures/СonvertibleFigures/ConvertibleCircle.cs
@@ -0,0 +1,35 @@
+﻿
+using System.Drawing;
+using DataStructures.Geometry;
+using System.Runtime.Serialization;
+
+namespace DataStructures.ConvertibleFigures
+{
+    [DataContract(Name = "Circle")]
+    public class ConvertibleCircle : ConvertibleFigure
+    {
+        // Координаты центра
+        [DataMember(Name = "Center")]
+        public Point2d center;
+
+        // Длина радиуса
+        [DataMember(Name = "Radius")]
+        public double radius;
+
+
+        public ConvertibleCircle(Point2d center, double radius, double angle)
+        {
+            this.center = center;
+            this.radius = radius;
+            this.angle = angle;
+
+            Height = radius * 2;
+            Width = radius * 2;
+
+            double px = center.X - radius;
+            double py = center.Y - radius;
+
+            position = new Point2d(px, py);
+        }
+    }
+}
